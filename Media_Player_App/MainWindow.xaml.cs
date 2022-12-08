659ee097d1bb3196cb8c22576f603fd959cc96b9
iamduy17 <thaiduydo17@gmail.com>
2022-12-08 23:42:24 +0700

    MP-4: Layout - Mock data, sẽ update thêm

--- Media_Player_App/MainWindow.xaml.cs
+++ Media_Player_App/MainWindow.xaml.cs
@@ -24,5 +24,85 @@ namespace Media_Player_App
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void New_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlaylistComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Clear_All_Playlist_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Add_Playlist_Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Remove_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Previous_Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Play_Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shuffle_Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
