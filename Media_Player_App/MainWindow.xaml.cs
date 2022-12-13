8e96acd06344fdf47f8ebc78be354bd79fa5b7ad
c0ngth4nk <congthanhtr>
2022-12-14 00:16:57 +0700

    MP-10, MP-11, MP-12: Play một file media, Play in suffle mode, Save a Playlist Và một vài chức năng phụ: Add file to list media, load playlist, clear playlist

--- Media_Player_App/MainWindow.xaml.cs
+++ Media_Player_App/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Win32;
+﻿using HandyControl.Data;
+using HandyControl.Tools.Extension;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -13,16 +19,34 @@ namespace Media_Player_App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool isPlaying = false;
+        private bool isMediaPlaying = false;
+        private bool isMediaSuffle = false;
+        private bool isMediaNewFile = false;
+        private Media CurrentMedia = null;
+        private ObservableCollection<Media> _PlayLists;
+        private ObservableCollection<String> _PlayListComboBox;
+        private const string PlayListFolder = "PlayList";
+
         public MainWindow()
         {
             InitializeComponent();
-            isPlaying= true;
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            // init flag for tracking media player's state
+            isMediaPlaying = false;
+            isMediaSuffle = true;
 
+            // init playlist
+            _PlayLists = new ObservableCollection<Media>();
+            Playlists.ItemsSource = _PlayLists;
+            Playlists.SelectionMode = System.Windows.Controls.SelectionMode.Single;
+
+            // init playlist combo box
+            _PlayListComboBox = new ObservableCollection<string>(GetAllJsonFile(PlayListFolder).Select(c => c.getFileName()).OrderBy(c => c).ToList());
+            PlaylistComboBox.ItemsSource = _PlayListComboBox;
         }
 
         private void New_File_Button_Click(object sender, RoutedEventArgs e)
@@ -31,35 +55,190 @@ namespace Media_Player_App
             if (screen.ShowDialog() == true)
             {
                 var _currentPlaying = screen.FileName;
-                //this.Title = $"Opened: {_shortName}";
-                media.Source = new Uri(_currentPlaying, UriKind.Absolute);
-                media.Play();
-                //media.Stop();
-
-                //_timer = new DispatcherTimer();
-                //_timer.Interval = new TimeSpan(0, 0, 0, 1, 0); ;
-                //_timer.Tick += _timer_Tick;
+                FileInfo fileInfo = new FileInfo(_currentPlaying);
+                Media newMedia = new Media()
+                {
+                    Name = fileInfo.Name,
+                    Singer = "Truong Cong Thanh",
+                    ImagePath = Directory.GetCurrentDirectory() + @"/Images/thanh.png",
+                    FullPath = new Uri(_currentPlaying)
+                };
+                var isDuplicate = _PlayLists.Where(c => c.FullPath == newMedia.FullPath && c.Name == newMedia.Name).ToArray();
+                if (isDuplicate == null || isDuplicate.Length <= 0)
+                {
+                    _PlayLists.Add(newMedia);
+                    media.Source = new Uri(_currentPlaying, UriKind.Absolute);
+                    #region set change in UI
+                    // if add new song, play it, no check state
+                    media.Play();
+                    isMediaPlaying = true;
+                    UpdatePlayButton();
+                    #endregion
+                    CurrentMedia = newMedia;
+                    #region set selected item in playlist
+                    //isMediaNewFile = true;
+                    Playlists.SelectedItem = newMedia;
+                    //isMediaNewFile = false;
+                    #endregion
+                }
             }
         }
 
         private void PlaylistComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            try
+            {
+                var playListSelected = PlaylistComboBox.SelectedValue;
+                if (playListSelected == null)
+                {
+                    return;
+                }
+                var playListDirectory = Directory.GetCurrentDirectory() + $"\\{PlayListFolder}\\{playListSelected}.json";
+                var playListContent = File.ReadAllText(playListDirectory);
+                if (playListContent != null) 
+                {
+                    var jsonToPlayList = JsonSerializer.Deserialize<ObservableCollection<Media>>(playListContent);
+                    if (jsonToPlayList != null) 
+                    {
+                        _PlayLists = new ObservableCollection<Media>();
+                        foreach (var media in jsonToPlayList)
+                        {
+                            _PlayLists.Add(media);
+                        }
+                    }
+                }
+                Playlists.ItemsSource = _PlayLists;
+                AddPlaylist.Text = playListSelected.ToString();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException ?? ex);
+            }
         }
 
         private void Clear_All_Files_In_Playlist_Btn_Click(object sender, RoutedEventArgs e)
         {
-
+            _PlayLists.Clear();
+            media.Source = null;
         }
 
         private void Add_Playlist_Button_CLick(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                var playListName = AddPlaylist.Text;
+                if (!string.IsNullOrWhiteSpace(playListName))
+                {
+                    if (_PlayLists != null && _PlayLists.Count > 0)
+                    {
+                        var playListFileName = playListName + ".json";
+                        var playListDirectory = Directory.GetCurrentDirectory() + $@"\\{PlayListFolder}\\";
+                        var playListJson = JsonSerializer.Serialize(_PlayLists);
+                        SaveJson(playListDirectory + playListFileName, playListJson);
+                        if (!_PlayListComboBox.Contains(playListName))
+                        {
+                            _PlayListComboBox.Add(playListName);
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Let's choose media file for your playlist firstly!");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Please name your playlist");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException ?? ex);
+            }
+        }
+        private void SaveJson(string filePath, string data, bool isOverride = false)
+        {
+            try
+            {
+                var folderName = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(filePath));
+                if (folderName == null)
+                {
+                    throw new Exception("Folder name not existed");
+                }
+                var listFile = GetAllJsonFile(folderName);
+                var fileName = System.IO.Path.GetFileName(filePath);
+                if (!listFile.Contains(fileName, StringComparer.OrdinalIgnoreCase) || isOverride == true)
+                {
+                    File.WriteAllText(filePath, data);
+                    HandyControl.Controls.MessageBox.Show(new MessageBoxInfo()
+                    {
+                        Message = "Save PlayList Successfully",
+                        Caption = "Save PlayList",
+                        Button = MessageBoxButton.OK,
+                    });
+                }
+                else
+                {
+                    MessageBoxResult action = HandyControl.Controls.MessageBox.Show(new MessageBoxInfo()
+                    {
+                        Message = "PlayList's name existed. Do you want to override it?",
+                        Caption = "Save PlayList",
+                        Button = MessageBoxButton.YesNo,
+                    });
+                    if (action == MessageBoxResult.Yes)
+                    {
+                        File.WriteAllText(filePath, data);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                System.IO.Directory.CreateDirectory(filePath);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException ?? ex);
+            }
+        }
+        private List<string> GetAllJsonFile(string FolderName)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\" + FolderName + "\\");
+                FileInfo[] fi = di.GetFiles("*.json");
+                if (fi != null && fi.Length > 0)
+                {
+                    foreach (var file in fi)
+                    {
+                        result.Add(file.Name);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                System.IO.Directory.CreateDirectory(Directory.GetCurrentDirectory() + $"\\{FolderName}\\");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex.InnerException ?? ex);
+            }
+            return result;
         }
 
         private void PlayList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            var selectedMedia = (Media)Playlists.SelectedItem;
+            if (selectedMedia != null)
+            {
+                if (selectedMedia.FullPath != null)
+                {
+                    media.Source = selectedMedia.FullPath;
+                    media.Play();
+                    isMediaPlaying = true;
+                    UpdatePlayButton();
+                    CurrentMedia = selectedMedia;
+                }
+            }
         }
 
         private void Remove_File_Button_Click(object sender, RoutedEventArgs e)
@@ -99,37 +278,73 @@ namespace Media_Player_App
 
         private void Play_Button_CLick(object sender, RoutedEventArgs e)
         {
-            try
+            if (isMediaPlaying)
             {
-                if (media.Source != null)
-                {
-                    if (isPlaying)
-                    {
-                        media.Pause();
-                        isPlaying = false;
-                        IsPlaying.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Play;
-                    }
-                    else
-                    {
-                        media.Play();
-                        isPlaying = true;
-                        IsPlaying.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Pause;
-                    }
-                }
+                media.Pause();
+                isMediaPlaying = false;
             }
-            catch(Exception ex) {
-                throw new Exception(ex.Message, ex.InnerException?? ex);
+            else
+            {
+                media.Play();
+                isMediaPlaying = true;
             }
+            UpdatePlayButton();
         }
 
         private void Next_Button_CLick(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Shuffle_Button_CLick(object sender, RoutedEventArgs e)
         {
+            if (isMediaSuffle)
+            {
+                isMediaSuffle = false;
+                IsSuffle.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.ShuffleDisabled;
+            }
+            else
+            {
+                IsSuffle.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Shuffle;
+                isMediaSuffle = true;
+            }
+            UpdatePlayButton();
+        }
+        private void UpdatePlayButton()
+        {
+            if (isMediaPlaying)
+            {
+                IsPlaying.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Pause;
+            }
+            else
+            {
+                IsPlaying.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Play;
+            }
+        }
 
+        private void media_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            var currentMediaIndex = Playlists.SelectedIndex;
+            int mediaIndex = -1;
+
+            if (isMediaSuffle)
+            {
+                Random randInt = new Random();
+                mediaIndex = randInt.Next(0, _PlayLists.Count - 1);
+                while (currentMediaIndex == mediaIndex)
+                {
+                    mediaIndex = randInt.Next(0, _PlayLists.Count - 1);
+                }
+            }
+            else
+            {
+                mediaIndex = currentMediaIndex++;
+            }
+            if (mediaIndex != -1)
+            {
+                media.Source = _PlayLists[mediaIndex].FullPath;
+                media.Play();
+            }
         }
     }
 }
---
+++ Media_Player_App/Media.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Media_Player_App
+{
+    public class Media : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string? Name { get; set; }
+        public string? Singer { get; set; }
+        public string? ImagePath { get; set; }
+        public Uri? FullPath { get; set; }
+    }
+}
